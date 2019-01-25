10f5ebc5640240da5f6dddbf25b191d2ddb473f2
jonva12 <jonva12@hotmail.com>
2019-01-25 13:57:22 +0100

    Inicio guzzle

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use GuzzleHttp\Client;
 
 Route::get('/', function () {
+
+$client = new Client([
+    // Base URI is used with relative requests
+    'base_uri' => 'https://swapi.co/api/',
+    // You can set any number of default request options.
+    'timeout'  => 2.0,
+]);
+	$response = $client->request('GET', 'planets');
     return view('welcome');
 });
