dfe118923451fbfdab92f7e539312b6e49e98a1d
jonva12 <jonva12@hotmail.com>
2019-01-29 09:51:39 +0100

    terminada api guzzleh

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,11 @@ Route::get('/', function () {
 
 $client = new Client([
     // Base URI is used with relative requests
-    'base_uri' => 'https://swapi.co/api/',
+    'base_uri' => 'https://jsonplaceholder.typicode.com',
     // You can set any number of default request options.
     'timeout'  => 2.0,
 ]);
-	$response = $client->request('GET', 'planets');
-    return view('welcome');
+	$response = $client->request('GET', 'posts');
+	$posts=json_decode($response->getBody()->getContents());
+    return view('index',compact('posts'));
 });
